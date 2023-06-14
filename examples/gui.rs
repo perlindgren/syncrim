a34caa00232ab8339dcf11754746a6b4a1a9d641
Per Lindgren <per.lindgren@ltu.se>
2023-06-14 21:59:46 +0200

    adder

--- examples/gui.rs
+++ examples/gui.rs
@@ -5,25 +5,37 @@ use syncrim::{
 };
 
 fn main() {
-    let r1 = Register {
-        id: "r1".to_string(),
-        pos: (100.0, 20.0),
-        r_in: Input {
+    let add1 = Add {
+        id: "add1".to_string(),
+        pos: (200.0, 75.0),
+        a_in: Input {
             id: "c1".to_string(),
             index: 0,
         },
+
+        b_in: Input {
+            id: "c2".to_string(),
+            index: 0,
+        },
     };
-    let r1 = Rc::new(r1);
+    let add1 = Rc::new(add1);
 
     let c1 = Constant {
         id: "c1".to_string(),
-        pos: (100.0, 40.0),
-        value: 2,
+        pos: (100.0, 50.0),
+        value: 1,
     };
     let c1 = Rc::new(c1);
 
+    let c2 = Constant {
+        id: "c2".to_string(),
+        pos: (100.0, 100.0),
+        value: 2,
+    };
+    let c2 = Rc::new(c2);
+
     let cs = ComponentStore {
-        store: vec![r1, c1],
+        store: vec![add1, c1, c2],
     };
 
     println!("--- store id:s");
