6caeaa747f44abe66dc70f30a85d1e6bd3e876bd
Per Lindgren <per.lindgren@ltu.se>
2023-07-04 18:58:18 +0200

    features wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 pub mod common;
 pub mod component_store;
 pub mod components;
-pub mod gui_vizia;
 pub mod simulator;
+
+// Vizia frontend
+#[cfg(feature = "vizia")]
+pub mod gui_vizia;
+
+#[cfg(feature = "vizia")]
 pub use vizia;
