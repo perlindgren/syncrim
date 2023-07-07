26593c840cb88ecb0c57df38e11e39cfe7a462e1
Per Lindgren <per.lindgren@ltu.se>
2023-07-07 10:52:45 +0200

    wip

--- src/components/mem.rs
+++ src/components/mem.rs
@@ -3,6 +3,7 @@ use num_enum::IntoPrimitive;
 use num_enum::TryFromPrimitive;
 use serde::{Deserialize, Serialize};
 
+use std::borrow::BorrowMut;
 use std::{cell::RefCell, collections::HashMap, convert::TryFrom};
 
 #[derive(Serialize, Deserialize)]
@@ -12,10 +13,14 @@ pub struct Mem {
     pub width: f32,
     pub height: f32,
 
+    // configuration
+    pub big_endian: bool,
+
     // ports
     pub data: Input,
     pub addr: Input,
     pub ctrl: Input,
+    pub sign_extend: Input,
     pub size: Input,
 
     // memory
@@ -23,15 +28,117 @@ pub struct Mem {
     // later history... tbd
 }
 
-type Memory = RefCell<HashMap<usize, u8>>;
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Memory {
+    bytes: RefCell<HashMap<usize, u8>>,
+}
 
-#[derive(Copy, Clone, Debug, IntoPrimitive, TryFromPrimitive)]
-#[repr(u8)] // Unfortunately Rust does not allow Signal here, we need to cast manually
-pub enum DataSize {
-    U8,
-    U16,
-    U32,
-    U64,
+impl Memory {
+    fn align(&self, addr: usize, size: usize) -> Signal {
+        (addr % size == 0) as Signal
+    }
+
+    fn read(&self, addr: usize, size: usize, sign_extend: bool, big_endian: bool) -> Signal {
+        let data: Vec<u8> = (0..size)
+            .into_iter()
+            .map(|i| *self.bytes.borrow().get(&(addr + i)).unwrap_or(&0))
+            .collect();
+
+        let data = data.as_slice();
+
+        match size {
+            1 => {
+                if sign_extend {
+                    data[0] as i8 as Signal
+                } else {
+                    data[0] as u8 as Signal
+                }
+            }
+            2 => {
+                if sign_extend {
+                    if big_endian {
+                        i16::from_be_bytes(data.try_into().unwrap()) as Signal
+                    } else {
+                        i16::from_le_bytes(data.try_into().unwrap()) as Signal
+                    }
+                } else {
+                    if big_endian {
+                        u16::from_be_bytes(data.try_into().unwrap()) as Signal
+                    } else {
+                        u16::from_le_bytes(data.try_into().unwrap()) as Signal
+                    }
+                }
+            }
+            4 => {
+                if sign_extend {
+                    if big_endian {
+                        i32::from_be_bytes(data.try_into().unwrap()) as Signal
+                    } else {
+                        i32::from_le_bytes(data.try_into().unwrap()) as Signal
+                    }
+                } else {
+                    if big_endian {
+                        u32::from_be_bytes(data.try_into().unwrap()) as Signal
+                    } else {
+                        u32::from_le_bytes(data.try_into().unwrap()) as Signal
+                    }
+                }
+            }
+            _ => panic!("illegal sized memory operation"),
+        }
+    }
+
+    fn write(&self, addr: usize, size: usize, big_endian: bool, data: Signal) {
+        match size {
+            1 => {
+                self.bytes.borrow_mut().insert(addr, data as u8);
+            }
+            2 => {
+                if big_endian {
+                    (data as u16)
+                        .to_be_bytes()
+                        .iter()
+                        .enumerate()
+                        .for_each(|(i, bytes)| {
+                            self.bytes.borrow_mut().insert(addr + i, *bytes);
+                        })
+                } else {
+                    (data as u16)
+                        .to_le_bytes()
+                        .iter()
+                        .enumerate()
+                        .for_each(|(i, bytes)| {
+                            self.bytes.borrow_mut().insert(addr + i, *bytes);
+                        })
+                }
+            }
+
+            4 => {
+                if big_endian {
+                    (data as u32)
+                        .to_be_bytes()
+                        .iter()
+                        .enumerate()
+                        .for_each(|(i, bytes)| {
+                            self.bytes.borrow_mut().insert(addr + i, *bytes);
+                        })
+                } else {
+                    (data as u32)
+                        .to_le_bytes()
+                        .iter()
+                        .enumerate()
+                        .for_each(|(i, bytes)| {
+                            self.bytes.borrow_mut().insert(addr + i, *bytes);
+                        })
+                }
+            }
+            _ => {
+                panic!("illegal sized memory operation")
+            }
+        };
+
+        unimplemented!()
+    }
 }
 
 #[derive(Copy, Clone, Debug, IntoPrimitive, TryFromPrimitive)]
@@ -67,23 +174,22 @@ impl Component for Mem {
         let ctrl = simulator.get_input_val(&self.ctrl);
         let ctrl = MemCtrl::try_from(ctrl as u8).unwrap();
         let size = simulator.get_input_val(&self.size);
-        let size = DataSize::try_from(size as u8).unwrap();
 
-        match ctrl {
-            MemCtrl::Read => {
-                println!("read addr {:?} size {:?}", addr, size);
-                let data = *self.memory.borrow().get(&(addr as usize)).unwrap_or(&0);
-                simulator.set_id_index(&self.id, 0, data as Signal)
-            }
-            MemCtrl::Write => {
-                println!("write addr {:?} size {:?}", addr, size);
-                self.memory.borrow_mut().insert(addr as usize, data as u8);
-            }
-            MemCtrl::None => {
-                println!("no read/write");
-            }
-        }
-        println!("memory {:?}", self.memory);
+        // match ctrl {
+        //     MemCtrl::Read => {
+        //         println!("read addr {:?} size {:?}", addr, size);
+        //         let data = *self.memory.borrow().get(&(addr as usize)).unwrap_or(&0);
+        //         simulator.set_id_index(&self.id, 0, data as Signal)
+        //     }
+        //     MemCtrl::Write => {
+        //         println!("write addr {:?} size {:?}", addr, size);
+        //         self.memory.borrow_mut().insert(addr as usize, data as u8);
+        //     }
+        //     MemCtrl::None => {
+        //         println!("no read/write");
+        //     }
+        // }
+        // println!("memory {:?}", self.memory);
     }
 }
 
@@ -102,20 +208,27 @@ mod test {
                 Rc::new(ProbeOut::new("addr")),
                 Rc::new(ProbeOut::new("ctrl")),
                 Rc::new(ProbeOut::new("size")),
+                Rc::new(ProbeOut::new("sign_extend")),
                 Rc::new(Mem {
                     id: "mem".to_string(),
                     pos: (0.0, 0.0),
                     width: 0.0,
                     height: 0.0,
 
+                    // configuration
+                    big_endian: false,
+
                     // ports
                     data: Input::new("data", 0),
                     addr: Input::new("addr", 0),
                     ctrl: Input::new("ctrl", 0),
                     size: Input::new("size", 0),
+                    sign_extend: Input::new("sign_extend", 0),
 
                     // memory
-                    memory: RefCell::new(HashMap::new()),
+                    memory: Memory {
+                        bytes: RefCell::new(HashMap::new()),
+                    },
                     // later history... tbd
                 }),
             ],
@@ -139,7 +252,7 @@ mod test {
         simulator.set_id_index("data", 0, 42);
         simulator.set_id_index("addr", 0, 4);
         simulator.set_id_index("ctrl", 0, MemCtrl::Write as Signal);
-        simulator.set_id_index("size", 0, DataSize::U8 as Signal);
+        simulator.set_id_index("size", 0, 1); // byte
 
         println!("sim_state {:?}", simulator.sim_state);
 
@@ -154,7 +267,7 @@ mod test {
         println!("<setup for read byte from addr 4>");
 
         simulator.set_id_index("ctrl", 0, MemCtrl::Read as Signal);
-        simulator.set_id_index("size", 0, DataSize::U8 as Signal);
+        simulator.set_id_index("size", 0, 1);
 
         simulator.clock(&mut clock);
 
