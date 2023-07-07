e7ff6a95f06fa9139efbee278943b230e1c03ffe
Per Lindgren <per.lindgren@ltu.se>
2023-07-07 22:48:29 +0200

    remove pub use for gui components

--- src/gui_egui/components/mod.rs
+++ src/gui_egui/components/mod.rs
@@ -7,13 +7,3 @@ mod probe_out;
 mod register;
 mod sext;
 mod wire;
-
-pub use add::*;
-pub use constant::*;
-pub use mem::*;
-pub use mux::*;
-pub use probe::*;
-pub use probe_out::*;
-pub use register::*;
-pub use sext::*;
-pub use wire::*;
