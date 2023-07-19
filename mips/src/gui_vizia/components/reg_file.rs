723cee90628becf7c84d496f246df5953f7a8fd6
Per Lindgren <per.lindgren@ltu.se>
2023-07-19 17:28:28 +0200

    pan

--- mips/src/gui_vizia/components/reg_file.rs
+++ mips/src/gui_vizia/components/reg_file.rs
@@ -4,11 +4,40 @@ use syncrim::{
     common::ViziaComponent, gui_vizia::tooltip::new_component_tooltip, vizia::prelude::*,
 };
 
+use log::*;
+
 #[typetag::serde]
 impl ViziaComponent for RegFile {
     // create view
+    fn left_view(&self, cx: &mut Context) {
+        trace!("---- Create Left Instr View");
+
+        View::build(
+            RegFileView {
+                registers: self.registers.clone(),
+            },
+            cx,
+            |cx| {
+                Label::new(cx, "Register File")
+                    .left(Pixels(10.0))
+                    .top(Pixels(10.0));
+
+                ScrollView::new(cx, 0.0, 0.0, false, true, |cx| {
+                    for i in RegStore::range() {
+                        let item = RegFileView::registers
+                            .map(move |reg| reg.0.borrow().get(i as usize).copied().unwrap());
+                        Label::new(cx, item);
+                    }
+                })
+                // .size(Units::Pixels(300.0))
+                .class("bg-default");
+            },
+        );
+    }
+
+    // create view
     fn view(&self, cx: &mut Context) {
-        println!("---- Create RegFile View");
+        trace!("---- Create RegFile View");
         View::build(
             RegFileView {
                 registers: self.registers.clone(),
