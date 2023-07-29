bbac3810b0beb6f728becc3da098eff70ff3d5c5
Per Lindgren <per.lindgren@ltu.se>
2023-07-29 22:35:50 +0200

    probe_out

---
+++ src/components/probe_out.rs
@@ -0,0 +1,34 @@
+use crate::common::{Component, Id, OutputType, Ports};
+use log::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct ProbeOut {
+    pub(crate) id: Id,
+}
+
+#[typetag::serde]
+impl Component for ProbeOut {
+    fn to_(&self) {
+        trace!("ProbeOut");
+    }
+
+    fn get_id_ports(&self) -> (Id, Ports) {
+        (
+            self.id.clone(),
+            Ports::new(
+                // Constants do not take any inputs
+                vec![],
+                OutputType::Combinatorial,
+                // Single output value
+                vec!["out"],
+            ),
+        )
+    }
+}
+
+impl ProbeOut {
+    pub fn new(id: &str) -> Self {
+        ProbeOut { id: id.into() }
+    }
+}
---
+++ src/gui_vizia/components/probe_out.rs
@@ -0,0 +1,4 @@
+use crate::{components::ProbeOut, gui_vizia::ViziaComponent};
+
+#[typetag::serde]
+impl ViziaComponent for ProbeOut {}
