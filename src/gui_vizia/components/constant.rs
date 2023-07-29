9696c3b4bf0cb203f29964ea077b9fb116cbe3ce
Per Lindgren <per.lindgren@ltu.se>
2023-07-29 19:57:05 +0200

    fix

--- src/gui_vizia/components/constant.rs
+++ src/gui_vizia/components/constant.rs
@@ -4,25 +4,11 @@ use crate::{
 };
 use vizia::prelude::*;
 
-// #[typetag::serde]
-// impl ViziaComponent for Constant {
-//     // create view
-//     fn view<'a>(&self, cx: &'a mut Context) -> Handle<'a, V> {
-//         V::new(cx, self.get_id_ports(), |cx| {
-//             Label::new(cx, &format!("{}", self.value))
-//         })
-//         .left(Pixels(self.pos.0 - 10.0))
-//         .top(Pixels(self.pos.1 - 10.0))
-//         .background_color(Color::lightblue())
-//     }
-// }
-
 #[typetag::serde]
 impl ViziaComponent for Constant {
     // create view
     fn view<'a>(&self, cx: &'a mut Context) -> Handle<'a, V> {
-        let x = self as &dyn Component;
-        V::new(cx, x, |cx| Label::new(cx, &format!("{}", self.value)))
+        V::new(cx, self, |cx| Label::new(cx, &format!("{}", self.value)))
             .left(Pixels(self.pos.0 - 10.0))
             .top(Pixels(self.pos.1 - 10.0))
             .background_color(Color::lightblue())
