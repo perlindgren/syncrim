d4bf2d2c70a40c108ef9e4d03861cf21cfeed9cb
Per Lindgren <per.lindgren@ltu.se>
2023-07-31 11:21:58 +0200

    polish

--- riscv/asm.s
+++ riscv/asm.s
@@ -3,6 +3,7 @@
 start:
     lw x0, 0(x0)
     sw x1, 0(x1)
+l:  beq zero, zero, l
 
 .section .data 
 a:  .byte 0,1,2,3
--- riscv/riscv_asm/build.rs
+++
@@ -1,5 +0,0 @@
-// fn main() {
-//     cc::Build::new().file("foo.c").file("bar.c").compile("foo");
-// }
-
-fn main() {}
