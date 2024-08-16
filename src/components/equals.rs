bfe23f10c9d81cd5301cf75b475ee8c0e0a249ee
malco <56635871+salon64@users.noreply.github.com>
2024-08-16 11:27:20 +0200

    added component to see if 2 signals are equal

---
+++ src/components/equals.rs
@@ -0,0 +1,89 @@
+//TODO: add so it can take undefiend number of inputs
+#[cfg(feature = "gui-egui")]
+use crate::common::EguiComponent;
+use crate::common::{
+    Component, Condition, Id, Input, InputPort, OutputType, Ports, SignalSigned, SignalUnsigned,
+    SignalValue, Simulator,
+};
+use log::*;
+use serde::{Deserialize, Serialize};
+use std::any::Any;
+use std::rc::Rc;
+pub const EQUAL_A_IN_ID: &str = "a_in";
+pub const EQUAL_B_IN_ID: &str = "b_in";
+
+pub const EQUAL_OUT_ID: &str = "out";
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Equal {
+    pub(crate) id: Id,
+    pub(crate) pos: (f32, f32),
+    pub(crate) a_in: Input,
+    pub(crate) b_in: Input,
+}
+
+#[typetag::serde]
+impl Component for Equal {
+    fn to_(&self) {
+        trace!("Equal");
+    }
+
+    fn get_id_ports(&self) -> (Id, Ports) {
+        (
+            self.id.clone(),
+            Ports::new(
+                vec![
+                    &InputPort {
+                        port_id: EQUAL_A_IN_ID.to_string(),
+                        input: self.a_in.clone(),
+                    },
+                    &InputPort {
+                        port_id: EQUAL_B_IN_ID.to_string(),
+                        input: self.b_in.clone(),
+                    },
+                ],
+                OutputType::Combinatorial,
+                vec![EQUAL_OUT_ID],
+            ),
+        )
+    }
+
+    // propagate addition to output
+    fn clock(&self, simulator: &mut Simulator) -> Result<(), Condition> {
+        // get input values
+        let a_in: u32 = simulator.get_input_value(&self.a_in).try_into().unwrap();
+        let b_in: u32 = simulator.get_input_value(&self.b_in).try_into().unwrap();
+
+        let result: u32 = (a_in == b_in) as u32;
+
+        simulator.set_out_value(&self.id, EQUAL_OUT_ID, SignalValue::Data(result));
+        Ok(())
+    }
+
+    fn set_id_port(&mut self, target_port_id: Id, new_input: Input) {
+        match target_port_id.as_str() {
+            EQUAL_A_IN_ID => self.a_in = new_input,
+            EQUAL_B_IN_ID => self.b_in = new_input,
+            _ => (),
+        }
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+}
+
+impl Equal {
+    pub fn new(id: &str, pos: (f32, f32), a_in: Input, b_in: Input) -> Self {
+        Equal {
+            id: id.to_string(),
+            pos,
+            a_in,
+            b_in,
+        }
+    }
+
+    pub fn rc_new(id: &str, pos: (f32, f32), a_in: Input, b_in: Input) -> Rc<Self> {
+        Rc::new(Equal::new(id, pos, a_in, b_in))
+    }
+}
---
+++ src/gui_egui/components/equal.rs
@@ -0,0 +1,80 @@
+use crate::common::{EguiComponent, Id, Ports, Simulator};
+use crate::components::Equal;
+use crate::gui_egui::editor::{EditorMode, EditorRenderReturn, GridOptions};
+use crate::gui_egui::gui::EguiExtra;
+use crate::gui_egui::helper::basic_component_gui;
+use egui::{Rect, Response, RichText, Ui, Vec2};
+
+#[typetag::serde]
+impl EguiComponent for Equal {
+    fn render(
+        &self,
+        ui: &mut Ui,
+        _context: &mut EguiExtra,
+        simulator: Option<&mut Simulator>,
+        offset: Vec2,
+        scale: f32,
+        clip_rect: Rect,
+        _editor_mode: EditorMode,
+    ) -> Option<Vec<Response>> {
+        // size of the component
+        let width = 100f32;
+        let height: f32 = 0f32;
+        basic_component_gui(
+            self,
+            &simulator,
+            ui.ctx(),
+            (width, height),
+            offset,
+            scale,
+            clip_rect,
+            |ui| {
+                ui.centered_and_justified(|ui| {
+                    ui.label(RichText::new("Equal").size(12f32 * scale));
+                });
+            },
+            // This is a hack to stop the compiler from complaining
+            // will hopefully be optimized away
+            None::<Box<dyn FnOnce(&mut Ui)>>,
+        )
+    }
+
+    fn render_editor(
+        &mut self,
+        ui: &mut egui::Ui,
+        context: &mut EguiExtra,
+        simulator: Option<&mut Simulator>,
+        offset: egui::Vec2,
+        scale: f32,
+        clip_rect: egui::Rect,
+        _id_ports: &[(Id, Ports)],
+        _grid: &GridOptions,
+        editor_mode: EditorMode,
+    ) -> EditorRenderReturn {
+        self.render(
+            ui,
+            context,
+            simulator,
+            offset,
+            scale,
+            clip_rect,
+            editor_mode,
+        );
+        EditorRenderReturn {
+            delete: false,
+            resp: None,
+        }
+    }
+
+    fn set_pos(&mut self, pos: (f32, f32)) {
+        self.pos = pos;
+    }
+
+    fn get_pos(&self) -> (f32, f32) {
+        self.pos
+    }
+
+    fn top_padding(&self) -> f32 {
+        20f32
+    }
+}
