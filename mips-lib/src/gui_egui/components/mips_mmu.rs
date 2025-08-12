7f75eca644b367221cedd0bb902976b31e1c7393
Olle Ronstad <olle.ronstad@gmail.com>
2025-08-12 11:18:25 +0200

    added gui for mips mmu

--- mips-lib/src/gui_egui/components/mips_mmu.rs
+++ mips-lib/src/gui_egui/components/mips_mmu.rs
@@ -1,4 +1,4 @@
-use crate::components::{MipsMmu, MMU_ADDRESS_OUT_ID, MMU_COMPONENT_SELECT_OUT_ID};
+use crate::components::{MipsMmu, MMU_ADDRESS_IN_ID, MMU_IO_REG_SEL_OUT, MMU_IO_RE_OUT, MMU_IO_WE_OUT, MMU_MEM_ADDRESS_OUT_ID, MMU_MEM_RE_OUT, MMU_MEM_WE_OUT, MMU_READ_ENABLE_IN, MMU_TIMER_ADDRESS_OUT, MMU_TIMER_RE_OUT, MMU_TIMER_WE_OUT, MMU_WRITE_ENABLE_IN};
 use egui::{pos2, Pos2, Rect, Response, RichText, Ui, Vec2};
 use syncrim::common::{EguiComponent, Id, Input, Ports, Simulator};
 use syncrim::gui_egui::editor::{EditorMode, EditorRenderReturn, GridOptions};
@@ -68,20 +68,65 @@ impl EguiComponent for MipsMmu {
 
     fn ports_location(&self) -> Vec<(syncrim::common::Id, Pos2)> {
         let own_pos = Vec2::new(self.pos.0, self.pos.1);
+        
+        // helper function to place the output ports
+        fn out_pos(i: u32) -> Pos2 {
+            pos2(
+                -WIDTH / 2.0 + (i as f32 + 1.0) * WIDTH / 12.0, // there are twelve component ports, re, we, adrs for 4 components, 3*4=12
+                HEIGHT / 2.0 + M,
+            )
+        }
         // margin of component
         const M: f32 = 6.0;
         vec![
             (
-                crate::components::MMU_ADDRESS_IN_ID.to_string(),
-                pos2(-WIDTH / 2.0 - M, 0.0) + own_pos,
+                MMU_ADDRESS_IN_ID.to_string(),
+                pos2(- WIDTH / 3.0, -HEIGHT-M) + own_pos,
             ),
             (
-                crate::components::MMU_COMPONENT_SELECT_OUT_ID.to_string(),
-                pos2(0.0, -HEIGHT / 2.0 - M) + own_pos,
+                MMU_READ_ENABLE_IN.to_string(),
+                pos2(0.0, 0.0-HEIGHT - M) + own_pos,
             ),
             (
-                crate::components::MMU_ADDRESS_OUT_ID.to_string(),
-                pos2(WIDTH / 2.0 + M, 0.0) + own_pos,
+                MMU_WRITE_ENABLE_IN.to_string(),
+                pos2(WIDTH / 3.0, -HEIGHT+WIDTH) + own_pos,
+            ),
+            // TODO CP0 ports
+            (
+                MMU_MEM_ADDRESS_OUT_ID.to_string(),
+                out_pos(3) + own_pos,
+            ),
+            (
+                MMU_MEM_RE_OUT.to_string(),
+                out_pos(4) + own_pos,
+            ),
+            (
+                MMU_MEM_WE_OUT.to_string(),
+                out_pos(5) + own_pos,
+            ),
+            (
+                MMU_TIMER_ADDRESS_OUT.to_string(),
+                out_pos(6) + own_pos,
+            ),
+            (
+                MMU_TIMER_RE_OUT.to_string(),
+                out_pos(7) + own_pos,
+            ),
+            (
+                MMU_TIMER_WE_OUT.to_string(),
+                out_pos(8) + own_pos,
+            ),
+            (
+                MMU_IO_REG_SEL_OUT.to_string(),
+                out_pos(9) + own_pos,
+            ),
+            (
+                MMU_IO_RE_OUT.to_string(),
+                out_pos(10) + own_pos,
+            ),
+            (
+                MMU_IO_WE_OUT.to_string(),
+                out_pos(11) + own_pos,
             ),
         ]
     }
@@ -95,10 +140,29 @@ impl EguiComponent for MipsMmu {
 
         if id == self.address_in {
             Some(loc[0])
-        } else if id == Input::new(&self.id, MMU_COMPONENT_SELECT_OUT_ID) {
+        } else if id == self.re_in {
             Some(loc[1])
-        } else if id == Input::new(&self.id, MMU_ADDRESS_OUT_ID) {
+        } else if id == self.we_in {
             Some(loc[2])
+        // TODO CP0
+        } else if id == Input::new(&self.id, MMU_MEM_ADDRESS_OUT_ID) {
+            Some(loc[3])
+        } else if id == Input::new(&self.id, MMU_MEM_RE_OUT) {
+            Some(loc[4])
+        } else if id == Input::new(&self.id, MMU_MEM_WE_OUT) {
+            Some(loc[5])
+        } else if id == Input::new(&self.id, MMU_TIMER_ADDRESS_OUT) {
+            Some(loc[6])
+        } else if id == Input::new(&self.id, MMU_TIMER_RE_OUT) {
+            Some(loc[7])
+        } else if id == Input::new(&self.id, MMU_TIMER_WE_OUT) {
+            Some(loc[8])
+        } else if id == Input::new(&self.id, MMU_IO_REG_SEL_OUT) {
+            Some(loc[9])
+        } else if id == Input::new(&self.id, MMU_IO_RE_OUT) {
+            Some(loc[10])
+        } else if id == Input::new(&self.id, MMU_IO_WE_OUT) {
+            Some(loc[11])
         } else {
             None
         }
