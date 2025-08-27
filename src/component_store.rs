dc85231952471a295df08e2ea88a6e624223df5c
Olle Ronstad <olle.ronstad@gmail.com>
2025-08-27 13:36:53 +0200

    change component store to return result instead of panic

--- src/component_store.rs
+++ src/component_store.rs
@@ -1,20 +1,50 @@
 use crate::common::ComponentStore;
 
-use std::{fs::File, io::prelude::*, path::PathBuf};
+use std::{fmt::Display, fs::File, io::prelude::*, path::PathBuf};
 
 use log::*;
 
+#[derive(Debug)]
+pub enum ComponentStoreLoadError {
+    Json(serde_json::Error),
+    Io(std::io::Error, PathBuf)
+}
+impl Display for ComponentStoreLoadError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ComponentStoreLoadError::Json(error) => write!(f,"Error while decoding json: {}",error),
+            ComponentStoreLoadError::Io(error,path) => write!(f,"Error while reading file {:?} : {}",path,error),
+        }
+    }
+}
+
+impl std::error::Error for ComponentStoreLoadError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            ComponentStoreLoadError::Json(error) => Some(error),
+            ComponentStoreLoadError::Io(error, _) => Some(error),
+        }
+    }
+}
+
+impl From<serde_json::Error> for ComponentStoreLoadError {
+    fn from(value: serde_json::Error) -> Self {
+        Self::Json(value)
+    }
+}
+
 impl ComponentStore {
-    pub fn load(json: &str) -> Self {
-        serde_json::from_str(json).unwrap()
+    pub fn load(json: &str) -> Result<Self, serde_json::Error> {
+        serde_json::from_str(json)
     }
 
-    pub fn load_file(path: &PathBuf) -> Self {
-        let mut file = File::open(path).unwrap();
+    pub fn load_file(path: &PathBuf) -> Result<Self,ComponentStoreLoadError> {
+        let mut file = File::open(path).map_err(|err| ComponentStoreLoadError::Io(err, path.clone()))?;
         let mut json = String::new();
-        file.read_to_string(&mut json).unwrap();
+        file.read_to_string(&mut json).map_err(|err| ComponentStoreLoadError::Io(err, path.clone()))?;
 
-        ComponentStore::load(&json)
+        let cs = ComponentStore::load(&json)?;
+        Ok(cs)
     }
 
     pub fn save_file(&self, path: &PathBuf) {
